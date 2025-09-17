2cd32bf4adefa6e29f9806703480030407d13921
Mohammed-Asif04 <mohad.asif0407@gmail.com>
2025-09-18 00:19:52 +0530

    update

--- hello/main.go
+++ hello/main.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main()  {
-	fmt.Println("Hello Go Language")
+	fmt.Println("Hello Go Language");
 }
---
+++ types/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main()  {
+	// Strings
+
+	var nameOne string = "mario";
+	var nameTwo = "Asif"
+	var namethree string
+	fmt.Println(nameOne ,nameTwo,namethree);
+
+	nameOne = "Mohad"
+	namethree = "Go lang"
+	fmt.Println(nameOne ,nameTwo,namethree);
+
+	nameFour := "learning Go" // only valid in func and another way of declaration of variable
+	fmt.Println(nameFour);
+
+	// Numbers
+
+	
+}
