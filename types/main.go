6cea1f4f145e6a9739c786fd8d4b6094bec4585f
Mohammed-Asif04 <mohad.asif0407@gmail.com>
2025-09-18 23:22:20 +0530

    update

--- types/main.go
+++ types/main.go
@@ -3,12 +3,12 @@ package main
 import "fmt"
 
 func main()  {
+
 	// Strings
 
 	var nameOne string = "mario";
 	var nameTwo = "Asif"
 	var namethree string
-	fmt.Println(nameOne ,nameTwo,namethree);
 
 	nameOne = "Mohad"
 	namethree = "Go lang"
@@ -19,5 +19,19 @@ func main()  {
 
 	// Numbers
 
+	var ageOne int = 20
+	var ageTwo= 30
+	ageThree :=40
+
+	fmt.Println(ageOne,ageTwo,ageThree);
+
+	//  Bits and Memeory
+
+	// var numOne int8 = 25  // int(number) refers to all signed bits range 2^(number)/2 both plus and minus sign
+    // var numTwo int8 = -128
+	// var numThree uint8 = 250 // only take plus bits and remove minus bits range 
+
+	// var scoreOne float32 =25.98
+	// var scoreTwo float64= 2353253454363465645654.878
 	
 }
